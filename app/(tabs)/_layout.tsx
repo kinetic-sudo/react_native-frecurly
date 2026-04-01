32984b70aca3b7592fb2962653411cf3ed0b0028
kinetic4 <pratyushbhargava6@gmail.com>
2026-04-01 23:39:35 +0530

    Add new tabs for Subscriptions, Insights, and Settings in TabLayout; remove SubscriptionDetails component

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,6 +4,11 @@ import React from 'react'
 const TabLayout = () => (
     <Tabs screenOptions={{ headerShown: false }}>
         <Tabs.Screen name='index' options={{ title: 'Home' }}/>
+        <Tabs.Screen name='Subscriptions' options={{ title: 'Subscriptions' }}/>
+        <Tabs.Screen name='insights' options={{ title: 'Insights' }}/>
+        <Tabs.Screen name='settings' options={{ title: 'Settings' }}/>
+        <Tabs.Screen name='Subscriptions/[id]' options={{ href: null }}/>
+
     </Tabs>
 )
 
