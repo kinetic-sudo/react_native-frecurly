d4a35e3810d948896e53803a135284a7c5e1230f
kinetic4 <pratyushbhargava6@gmail.com>
2026-04-03 18:42:17 +0530

    Refactor home screen layout by adding user avatar and balance display; update styles for add button and user name

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,11 @@
+import { HOME_BALANCE, HOME_USER } from "@/constants/data";
+import { icons } from "@/constants/icons";
+import images from "@/constants/images";
 import "@/global.css";
-import { Link } from "expo-router";
+import { formatCurrency } from "@/lib/utils/CurrencyFormating";
+import dayjs from 'dayjs';
 import { styled } from 'nativewind';
-import { Text } from "react-native";
+import { Image, Pressable, Text, View } from "react-native";
 import { SafeAreaView as RnsafeAreaView } from "react-native-safe-area-context";
 
 const SafeAreaView = styled(RnsafeAreaView);
@@ -9,19 +13,30 @@ const SafeAreaView = styled(RnsafeAreaView);
 export default function App() {
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
-      <Text className="text-5xl font-sans-extrabold">
-        Home
-      </Text>
-      <Link href='/onboarding' className="mt-4 font-sans-bold rounded bg-primary text-white p-4">
-        Go to onboarding
-      </Link>
-      <Link href='/(auth)/Sign-in' className="mt-4 font-sans-bold rounded bg-primary text-white p-4">
-        Go to Sign in
-      </Link>
-      <Link href='/(auth)/Sign-up' className="mt-4 font-sans-bold rounded bg-primary text-white p-4">
-        Go to Sign up
-      </Link>
-  
+      <View className='home-header'>
+        <View className="home-user">
+          <Image source={images.avatar} className="home-avatar" />
+            <Text className='home-user-name'>
+              {HOME_USER.name}
+            </Text>
+        </View>
+        <Pressable className="home-add-btn" accessibilityRole="button">
+          <Image source={icons.plus} className="home-add-icon" />
+        </Pressable>
+      </View>
+      <View className="home-balance-card">
+        <Text className="home-balance-label"> 
+          Balance 
+        </Text>
+        <View className="home-balance-row">
+          <Text className="home-balance-amount">
+            { formatCurrency(HOME_BALANCE.amount)}
+          </Text>
+          <Text className="home-balance-date">
+            {dayjs(HOME_BALANCE.nextRenewalDate).format('MM/DD')}
+          </Text>
+        </View>
+      </View>
     </SafeAreaView>
   );
 }
